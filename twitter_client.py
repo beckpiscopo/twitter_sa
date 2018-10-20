97c6a4b402b22e2a5ad9d345835b2a62a97a5a1b
brittbec <hello@brittbec.com>
2018-10-20 18:58:24 -0500

    First Commit

---
+++ twitter_client.py
@@ -0,0 +1,31 @@
+import os
+import sys
+from tweepy import API
+from tweepy import OAuthHandler
+
+
+def get_twitter_auth():
+    ''' Setup Twitter Authentication
+
+    Return: tweety.OAuthHandler object
+    '''
+    try: 
+        consumer_key = os.environ['TWITTER_CONSUMER_KEY']
+        consumer_secret = os.environ['TWITTER_CONSUMER_SECRET']
+        access_token = os.environ['TWITTER_ACCESS_TOKEN']
+        access_secret = os.environ['TWitter_ACESS_SECRET']
+    except KeyError:
+        syst.stderr.write("Twitter_ + envirnoment variable not set\n")
+        sys.exit(1)
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_secret)
+
+def get_twitter_client(): 
+    ''' Setup Twitter API client. 
+
+        Return: tweepy API object
+    '''
+    auth = get_twitter_auth()
+    client= API(auth)
+    return client
+    
---
+++ twitter_get_home_timeline.py
@@ -0,0 +1,11 @@
+import tweepy
+
+from tweepy import Cursor
+from twitter_client import get_twitter_client
+
+if __name__ == '__main__': 
+    client = get_twitter_client()
+
+    for status in Cursor(client.home_timeline).items(10): 
+        # Process a single status 
+        print(status.text)
