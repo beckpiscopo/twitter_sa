79bb878449434b9a49467f6c03a19cef74db1dd4
brittbec <hello@brittbec.com>
2018-10-20 19:50:56 -0500

    Mined Tweets, added them to a .json file

--- twitter_get_home_timeline.py
+++ twitter_get_home_timeline.py
@@ -1,9 +1,11 @@
+import json
 from tweepy import Cursor
 from twitter_client import get_twitter_client
 
 if __name__ == '__main__': 
     client = get_twitter_client()
 
-    for status in Cursor(client.home_timeline).items(10): 
-        # Process a single status 
-        print(status.text)
+    with open('home_timeline.json', 'w') as f: 
+        for page in Cursor(client.home_timeline, count=200).pages(4):
+            for status in page:
+                f.write(json.dumps(status._json) + "\n")
